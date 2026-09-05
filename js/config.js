0d918225bb35dd9f90b7df9905c2486d777988a4
Quil02 <maulana02260@gmail.com>
2026-09-05 14:57:23 +0000

    refactor: rename GOOGLE_BOOK_API to GOOGLE_BOOK_API_BASE_URL

--- js/config.js
+++ js/config.js
@@ -1,10 +1,10 @@
 export const CONFIG = {
-  API_BASE_URL: '',
-  GOOGLE_BOOK_API: 'https://www.googleapis.com/books/v1/volumes',
-  LIBGEN_API_BASE_URL: 'https://libgen-api.onrender.com/books',
-  WS_BASE_URL: '',
-  TOKEN_KEY: 'bookstore_auth_token',
-  USER_KEY: 'bookstore_user_data'
+  API_BASE_URL: "",
+  WS_BASE_URL: "",
+  GOOGLE_BOOK_API_BASE_URL: "https://www.googleapis.com/books/v1/volumes",
+  LIBGEN_API_BASE_URL: "https://libgen-api.onrender.com/books",
+  TOKEN_KEY: "bookstore_auth_token",
+  USER_KEY: "bookstore_user_data",
 };
 
 export default CONFIG;
